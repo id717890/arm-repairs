0e702927d0e23a8669f52b1026183077a8452299
YusupovZamir <jusupovz@gmail.com>
2017-02-28 11:39:46 +0500

    реализована авторизация + миграция

--- sources/arm.web/Migrations/201702280549172_Init.Designer.cs
+++ sources/arm.web/Migrations/201702280549172_Init.Designer.cs
@@ -7,13 +7,13 @@ namespace arm_repairs_project.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class DataMigration : IMigrationMetadata
+    public sealed partial class Init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(DataMigration));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201702271732263_DataMigration"; }
+            get { return "201702280549172_Init"; }
         }
         
         string IMigrationMetadata.Source
